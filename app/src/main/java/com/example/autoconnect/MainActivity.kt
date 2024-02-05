ab1c7ac74a35f830406669178d748005b1c758d5
Luqman Master <P2655547@my365.dmu.ac.uk>
2024-02-05 18:27:02 +0000

    Added activities for the created pages

--- app/src/main/java/com/example/autoconnect/MainActivity.kt
+++ app/src/main/java/com/example/autoconnect/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.autoconnect
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonGoToMenu: Button = findViewById(R.id.buttonGoToMenu)
+        val buttonLeaveReview: Button = findViewById(R.id.buttonLeaveReview)
+        val buttonViewOrders: Button = findViewById(R.id.buttonViewOrders)
+        val buttonSignOut: Button = findViewById(R.id.buttonSignOut)
+
+        buttonGoToMenu.setOnClickListener {
+            //startActivity(Intent(this, MenuActivity::class.java))
+        }
+
+        buttonLeaveReview.setOnClickListener {
+            //startActivity(Intent(this, ReviewActivity::class.java))
+        }
+
+        buttonViewOrders.setOnClickListener {
+            //startActivity(Intent(this, OrdersActivity::class.java))
+        }
+
+        buttonSignOut.setOnClickListener {
+            startActivity(Intent(this, StartActivity::class.java))
+            finish()
+        }
+    }
+
+    @Suppress("MissingSuperCall")
+    override fun onBackPressed() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/autoconnect/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.autoconnect
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Delay and then start the main activity
+        Handler(Looper.myLooper()!!).postDelayed({
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 500)
+    }
+}
