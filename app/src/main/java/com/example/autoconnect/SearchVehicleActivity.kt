6e77ff8937160b17dfbb6091569b69fe4d783ac7
Luqman Master <P2655547@my365.dmu.ac.uk>
2024-02-21 22:07:12 +0000

    Added number plate type search button. Formatted VRN search results to display in a formatted way

--- app/src/main/java/com/example/autoconnect/SearchVehicleActivity.kt
+++ app/src/main/java/com/example/autoconnect/SearchVehicleActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -30,7 +31,7 @@ class SearchVehicleActivity : AppCompatActivity() {
             val client = OkHttpClient()
             val mediaType = "application/json".toMediaType()
             val vrn = editTextVRN.text.toString() // Get the VRN from editTextVRN
-            val body = "{\"registrationNumber\": \"$vrn\"}".toRequestBody(mediaType) // Use the VRN in the request body
+            val body = "{\"registrationNumber\": \"$vrn\"}".toRequestBody(mediaType)
             val request = Request.Builder()
                 .url("https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles")
                 .post(body)
@@ -41,11 +42,57 @@ class SearchVehicleActivity : AppCompatActivity() {
             GlobalScope.launch(Dispatchers.IO) {
                 val response = client.newCall(request).execute()
                 val responseBody = response.body?.string()
+
+                // Parse JSON response using Gson
+                val gson = Gson()
+                val vehicleInfo = gson.fromJson(responseBody, VehicleInfo::class.java)
+
+                // Construct formatted text to display in textViewVehicleDetails
+                val formattedText = StringBuilder().apply {
+                    append("Make: ${vehicleInfo.make}\n")
+                    append("Year of Manufacture: ${vehicleInfo.yearOfManufacture}\n")
+                    append("Engine Capacity: ${vehicleInfo.engineCapacity}\n")
+                    append("CO2 Emissions: ${vehicleInfo.co2Emissions}\n")
+                    append("Fuel Type: ${vehicleInfo.fuelType}\n")
+                    append("Tax Status: ${vehicleInfo.taxStatus}\n")
+                    append("Tax Due Date: ${vehicleInfo.taxDueDate}\n")
+                    append("MOT Status: ${vehicleInfo.motStatus}\n")
+                    append("Marked for Export: ${if (vehicleInfo.markedForExport) "Yes" else "No"}\n")
+                    append("Colour: ${vehicleInfo.colour}\n")
+                    append("Type Approval: ${vehicleInfo.typeApproval}\n")
+                    append("Date of Last V5C Issued: ${vehicleInfo.dateOfLastV5CIssued}\n")
+                    append("MOT Expiry Date: ${vehicleInfo.motExpiryDate}\n")
+                    append("Wheelplan: ${vehicleInfo.wheelplan}\n")
+                    append("Month of First Registration: ${vehicleInfo.monthOfFirstRegistration}\n")
+                }.toString()
+
+
                 runOnUiThread {
-                    // Update UI with response data
-                    textViewVehicleDetails.text = responseBody
+                    // Update UI with formatted text
+                    textViewVehicleDetails.text = formattedText
                 }
             }
         }
     }
 }
+
+// Data class to represent vehicle information
+data class VehicleInfo(
+    val registrationNumber: String,
+    val taxStatus: String,
+    val taxDueDate: String,
+    val motStatus: String,
+    val make: String,
+    val yearOfManufacture: Int,
+    val engineCapacity: Int,
+    val co2Emissions: Int,
+    val fuelType: String,
+    val markedForExport: Boolean,
+    val colour: String,
+    val typeApproval: String,
+    val dateOfLastV5CIssued: String,
+    val motExpiryDate: String,
+    val wheelplan: String,
+    val monthOfFirstRegistration: String
+)
+
