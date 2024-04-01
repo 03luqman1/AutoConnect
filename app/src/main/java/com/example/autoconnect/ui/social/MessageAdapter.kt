612b5ac9d5ff369c4219c3e4e5ab9e83537be7d8
Luqman Master <P2655547@my365.dmu.ac.uk>
2024-04-01 18:19:36 +0100

    chats are coloured to identify if its the user that sent it.

--- app/src/main/java/com/example/autoconnect/ui/social/MessageAdapter.kt
+++ app/src/main/java/com/example/autoconnect/ui/social/MessageAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.autoconnect.ui.social
 
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -33,12 +35,15 @@ class MessageAdapter(
         if (senderUsername == currentUserUsername) {
             // Set a different text color for the current user's message
             holder.messageTextView.backgroundTintList = ContextCompat.getColorStateList(holder.itemView.context, R.color.teal_700)
-
+            (holder.messageTextView.layoutParams as? LinearLayout.LayoutParams)?.gravity = Gravity.END
+            (holder.senderTextView.layoutParams as? LinearLayout.LayoutParams)?.gravity = Gravity.END
 
             //holder.messageTextView.setTextColor(ContextCompat.getColor(holder.itemView.context, R.color.black))
         } else {
 
             holder.messageTextView.backgroundTintList = ContextCompat.getColorStateList(holder.itemView.context, R.color.black)
+            (holder.messageTextView.layoutParams as? LinearLayout.LayoutParams)?.gravity = Gravity.START
+            (holder.senderTextView.layoutParams as? LinearLayout.LayoutParams)?.gravity = Gravity.START
         }
     }
 
